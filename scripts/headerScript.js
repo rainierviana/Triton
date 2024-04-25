335ffabed8351022b4b9313f3f258cba242c5581
rainierviana <rainier.viana@4bears.com.br>
2024-04-25 12:29:55 -0300

    Updated code with new breadcrumb item

--- scripts/headerScript.js
+++ scripts/headerScript.js
@@ -1,9 +1,9 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {fixedTopNavigation()};
 
-var header = document.getElementById("myHeader");
+var header = document.getElementById("fixedTopNavigation");
 var sticky = header.offsetTop;
 
-function myFunction() {
+function fixedTopNavigation() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
